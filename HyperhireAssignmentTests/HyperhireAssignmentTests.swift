982b88c8c5448bf039d27f89f3b674a721d600da
Ricky Wirawan <rickywrwn@gmail.com>
2024-12-06 16:57:49 +0700

    Initial Commit

---
+++ HyperhireAssignmentTests/HyperhireAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  HyperhireAssignmentTests.swift
+//  HyperhireAssignmentTests
+//
+//  Created by ricky wirawan on 06/12/24.
+//
+
+import Testing
+@testable import HyperhireAssignment
+
+struct HyperhireAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
